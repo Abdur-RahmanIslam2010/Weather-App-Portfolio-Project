3fc9fb4442bf30c1c8d08fc0a7cc0baa0b2d48ef
Abdur-RahmanIslam2010 <abdelrahmanislam116@gmail.com>
2025-11-27 22:45:22 +0200

    Add Adjustments
    I adjusted the API code a bit. I also changed the port so that I wouldn't confuse it with other project ports. CORS is also added.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(cors());
 
-app.get('/', async (req, res) => {
-    const data = await fetch("https://api.open-meteo.com/v1/forecast?latitude=40.7143&longitude=-74.006&daily=temperature_2m_min,temperature_2m_max,sunset,sunrise,weather_code&hourly=temperature_2m,relative_humidity_2m&timezone=America%2FNew_York");
-    const data_json = await data.json();
-    await console.log(data_json);
-    res.status(200).send(data_json);
+app.get('/api', (req, res) => {
+    fetch("https://api.open-meteo.com/v1/forecast?latitude=40.7143&longitude=-74.006&daily=temperature_2m_min,temperature_2m_max,sunset,sunrise,weather_code&hourly=temperature_2m,relative_humidity_2m&timezone=America%2FNew_York")
+    .then(response => response.json())
+    .then(data => {
+        res.send(data);
+    })
 });
 
 app.listen(port, (err) => {
