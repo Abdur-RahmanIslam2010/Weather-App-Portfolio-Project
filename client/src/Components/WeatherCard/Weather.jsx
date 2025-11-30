dd18fb85e1c6ef8717713995f7aa55e6657d85ab
Abdur-RahmanIslam2010 <abdelrahmanislam116@gmail.com>
2025-11-30 19:09:41 +0200

    Adjust Weather Cards code
    Adjusted the code for the React component that displays the weather data (Weather Cards) to be able to also display icons for more visualization. Minor styling details like padding, margin, and transitions are adjusted/added.

--- client/src/Components/WeatherCard/Weather.jsx
+++ client/src/Components/WeatherCard/Weather.jsx
@@ -1,9 +1,11 @@
 import './Weather.css';
 
 function WeatherCard(props) {
+    const iconUrl = new URL(props.icon, "http://localhost:8080/icons/");
     return (
         <div className='card'>
-            <h1>{props.date}</h1>
+            <h2>{props.date}</h2>
+            <img src={iconUrl.href} alt={props.weather} width={100} height={100}/>
             <p>{props.weather}</p>
         </div>
     );
